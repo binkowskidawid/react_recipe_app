03dea27e9b9799e14dfc43a2ba30d378114ec30c
Dawid Binkowski <d.k.binkowski@gmail.com>
2021-09-18 11:00:17 +0200

    change ingredients key value

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
 import { useEffect, useState } from "react";
 import Recipe from "./Recipe";
 import "./styles.css";
-import logo from"./logo.png"
+import logo from "./logo.png";
 
 const App = () => {
-  const APP_ID = "9e525a8d";
-  const APP_KEY = "a1e84e36ac87d3dc9cf25ff2dc50c738";
+	const APP_ID = "9e525a8d";
+	const APP_KEY = "a1e84e36ac87d3dc9cf25ff2dc50c738";
 
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+	const [recipes, setRecipes] = useState([]);
+	const [search, setSearch] = useState("");
+	const [query, setQuery] = useState("chicken");
 
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
+	useEffect(() => {
+		getRecipes();
+	}, [query]);
 
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
+	const getRecipes = async () => {
+		const response = await fetch(
+			`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+		);
 
-    const data = await response.json();
-    setRecipes(data.hits);
-  };
+		const data = await response.json();
+		setRecipes(data.hits);
+	};
 
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-  };
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+	};
 
-  const getSearch = (e) => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch("");
-  };
+	const getSearch = (e) => {
+		e.preventDefault();
+		setQuery(search);
+		setSearch("");
+	};
 
-  return (
-    <div className="App">
-<img className="logo" src={logo} alt="logo" />
-      <form onSubmit={getSearch} className="search-form">
-        <input
-          className="search-bar"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-      <div className="recipes">
-        {recipes.map((recipe) => (
-          <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={Math.floor(recipe.recipe.calories)}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<img className="logo" src={logo} alt="logo" />
+			<form onSubmit={getSearch} className="search-form">
+				<input
+					className="search-bar"
+					type="text"
+					value={search}
+					onChange={updateSearch}
+				/>
+				<button className="search-button" type="submit">
+					Search
+				</button>
+			</form>
+			<div className="recipes">
+				{recipes.map((recipe) => (
+					<Recipe
+						key={recipe.recipe.label}
+						title={recipe.recipe.label}
+						calories={Math.floor(recipe.recipe.calories)}
+						image={recipe.recipe.image}
+						ingredients={recipe.recipe.ingredients}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- src/Recipe.js
+++ src/Recipe.js
@@ -2,18 +2,18 @@ import React from "react";
 import style from "./recipe.module.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
-  return (
-    <div className={style.recipe}>
-      <h1 className={style.header}>{title}</h1>
-      <ol className={style.list}>
-        {ingredients.map((ingredient) => (
-          <li key={Math.random()}>{ingredient.text}</li>
-        ))}
-      </ol>
-      <img className={style.image} src={image} alt="" />
-      <p className={style.kcal}>{calories} kcal</p>
-    </div>
-  );
+	return (
+		<div className={style.recipe}>
+			<h1 className={style.header}>{title}</h1>
+			<ol className={style.list}>
+				{ingredients.map((ingredient, key) => (
+					<li key={key}>{ingredient.text}</li>
+				))}
+			</ol>
+			<img className={style.image} src={image} alt="" />
+			<p className={style.kcal}>{calories} kcal</p>
+		</div>
+	);
 };
 
 export default Recipe;
